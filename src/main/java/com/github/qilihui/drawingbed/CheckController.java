34463a0dba5dc8c9d3355b48e403dcfc924a669d
qilihui <1499910521@qq.com>
2021-08-14 21:37:41 +0800

    添加心跳检测

--- src/main/java/com/github/qilihui/drawingbed/CheckController.java
+++ src/main/java/com/github/qilihui/drawingbed/CheckController.java
@@ -1,5 +1,6 @@
 package com.github.qilihui.drawingbed;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2021/8/14 18:31
  */
 @RestController
+@Slf4j
 public class CheckController {
     @RequestMapping("/ping")
     public String ping(){
+        log.info("执行心跳检测");
         return "OK";
     }
 }
