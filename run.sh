f5be5060a058dc79d34d176163d27c2f3ffdf7fa
qilihui <1499910521@qq.com>
2021-08-14 18:09:50 +0800

    修改构建文件

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "开始打包"
+mvn clean package -Dmaven.test.skip=true
+app="./target/drawingbed-0.0.1-SNAPSHOT.jar"
+if [ ! -f $app ]; then
+    echo "error: 打包失败"
+    exit
+fi
+docker-composer up -d
+echo "打包完成"
+
